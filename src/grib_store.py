d1e01bc173541786b43d7a9d8d52e3cd89405ff3
Patryk Przekwas <pkprzekwas@gmail.com>
2017-04-30 08:45:00 +0200

    Refactored grib store. Deleted unused methods and singleton type added.

--- src/grib_store.py
+++ src/grib_store.py
@@ -4,38 +4,21 @@ from logger import logger
 from config import BASE_DIR
 
 
+class SingletonType(type):
+    def __call__(cls, *args, **kwargs):
+        try:
+            return cls.__instance
+        except AttributeError:
+            cls.__instance = super(SingletonType, cls).__call__(*args, **kwargs)
+            return cls.__instance
+
+
 class GribStore:
+    __metaclass__ = SingletonType
+
     def __init__(self):
         self.dir = os.path.join(BASE_DIR, 'gribs')
 
     @property
     def gribs(self):
         return sorted(os.listdir(self.dir))
-
-    def _last_grib(self):
-        if len(self.gribs) == 0:
-            logger.info('No files downloaded yet')
-            return
-        return self.gribs[0]
-
-    def _last_grib_splited(self):
-        grb = self._last_grib()
-        if grb is None:
-            return
-        return grb.split('_')
-
-    def last_gribs_date(self):
-        lgs =  self._last_grib_splited()
-        if lgs is None:
-            return '99999999'
-        return lgs[2]
-
-    def last_gribs_month(self):
-        date = self.last_gribs_date()
-        return date[:6]
-
-    def last_gribs_hour(self):
-        lgs = self._last_grib_splited()
-        if lgs is None:
-            return 25
-        return lgs[3][:2]
